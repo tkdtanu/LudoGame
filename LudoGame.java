fbbfd395929201a87c370d6fcc00f2bac6a4e10b
Tarun Kumar Das <tkdtanu@gmail.com>
2022-03-25 13:41:01 +0530

    Update LudoGame.java

--- LudoGame.java
+++ LudoGame.java
@@ -7,10 +7,12 @@ public class LudoGame {
     public static void main(String[] args) throws InterruptedException {
         PlayerDetail player=new PlayerDetail(4,1);
         Random random = new Random();
+
         Thread player1 = new LudoPlayer("Player1", 1,player, random);
         Thread player2 = new LudoPlayer("Player2", 2,player, random);
         Thread player3 = new LudoPlayer("Player3", 3,player, random);
         Thread player4 = new LudoPlayer("Player4", 4,player, random);
+
         player1.start();
         player2.start();
         player3.start();
@@ -19,10 +21,13 @@ public class LudoGame {
     }
 }
 class LudoPlayer extends Thread{
+
     private final PlayerDetail playerDetail;
     private final int playerId;
     private final Random random;
-    public LudoPlayer(String name,int playerId, PlayerDetail playerDetail, Random random) {
+
+    public LudoPlayer(String name,int playerId,
+                      PlayerDetail playerDetail, Random random) {
         super(name);
         this.playerDetail = playerDetail;
         this.playerId = playerId;
@@ -37,14 +42,17 @@ class LudoPlayer extends Thread{
                         while (this.playerId != this.playerDetail.getCurrentPlayerId()) {
                             this.playerDetail.wait();
                         }
+
                         System.out.println("Player :" + this.getName() + " Term");
                         Thread.sleep(500);
+
                         System.out.println("Rolling Dice.... And Here it came");
                         Thread.sleep(500);
+
                         int number = random.nextInt(7);
                         System.out.println(number);
-                        if(number != 6) {
 
+                        if(number != 6) {
                             if (this.playerDetail.getNoOfPlayer() == this.playerId) {
                                 this.playerDetail.setCurrentPlayerId(1);
                             } else {
